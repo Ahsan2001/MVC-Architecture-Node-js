f516586a143a2b7c8ca8d6eb36f11f6c3f588bb0
Ahsan2001 <ahsan2sabir@gmail.com>
2022-05-17 05:45:52 +0500

    update mvc

--- app.js
+++ app.js
@@ -1,22 +1,20 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 //bodyAllow
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-const router = require("./route/routes");
-
+app.use(cors());
 // security intailizing 
 const dotenv = require("dotenv");
 dotenv.config({ path: './config.env' });
 
+const router = require("./route/routes");
 
 // get database from mongoDB Server 
 require("./database/connection")
 const PORT = process.env.PORT;
-
 app.use(router);
-
 app.listen(PORT, () =>
     console.log(`server running on http://localhost:${PORT}`)
 );
